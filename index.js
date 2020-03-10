4ff2e371c58481431dfa6ab2c3d3bc137873c541
ceboulba <antoine@archipicture.fr>
2020-03-10 17:26:25 +0100

    1 file modified

--- index.js
+++ index.js
@@ -116,6 +116,26 @@ var createScene = function() {
 
   assetsManager.load();
 
+  scene.actionManager = new BABYLON.ActionManager(scene);
+
+// From 2D view to fullscreen VR
+scene.actionManager.registerAction(
+new BABYLON.ExecuteCodeAction({
+        trigger: BABYLON.ActionManager.OnKeyDownTrigger,
+        parameter: 's' //press "s" key
+    },
+    function () { vrHelper.enterVR(); }
+));
+
+// From fullscreenVR to 2D view
+scene.actionManager.registerAction(
+new BABYLON.ExecuteCodeAction({
+        trigger: BABYLON.ActionManager.OnKeyDownTrigger,
+        parameter: 'e' //press "e" key
+    },
+    function () { vrHelper.exitVR(); document.exitFullscreen();}
+));
+
   return scene;
 };
 
